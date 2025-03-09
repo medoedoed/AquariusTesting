4732f5df2ca1ab66615036e534faccb3d117fa34
medoed <medoedoded@yandex.ru>
2025-03-09 14:50:53 +0300

    error message fixed

--- src/main/java/logs/ErrorMessage.java
+++ src/main/java/logs/ErrorMessage.java
@@ -2,15 +2,6 @@ package logs;
 
 public class ErrorMessage extends Message {
     private static final String ANSI_RED = "\u001B[31m";
-    private static boolean isActive = true;
-
-    public static void disable() {
-        isActive = false;
-    }
-
-    public static void enable() {
-        isActive = true;
-    }
 
     public static void send(String messageBody) {
         if (!isActive) return;
--- src/main/java/logs/WarningMessage.java
+++ src/main/java/logs/WarningMessage.java
@@ -3,7 +3,6 @@ package logs;
 public class WarningMessage extends Message {
     private static final String ANSI_ORANGE = "\u001B[38;5;214m";
 
-
     public static void send(String messageBody) {
         if (!isActive) return;
 
